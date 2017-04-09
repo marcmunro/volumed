4207256d741f42fb072fb7faba407b67d3671c5b
Marc Munro <marc@bloodnok.com>
2017-04-09 16:58:48 -0700

    Configuration file handling done.

---
+++ src/config.c
@@ -0,0 +1,307 @@
+/*
+ *     Copyright (c) 2017 Marc Munro
+ *     Author:  Marc Munro
+ *     License: GPL V3
+ *
+ */
+
+#ifdef DOCONLY
+/*
+ * Put any mainpage documentation sections that we might need, in here.
+ */
+#else
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <glob.h>
+#include "volumed.h"
+
+
+/**
+ * @brief Open our config file, failing if we have an explicit config
+ * file option and it cannot be opened.
+ * 
+ * @param options (option_t *) Pointer to our options struct.
+ * @return (FILE *) the opened config file.
+ */
+static FILE *
+open_config_file(options_t *options, char **p_filename)
+{
+    FILE *f = NULL;
+    char *name;
+    
+    if (options->config_filename) {
+	/* Explicit config file was specified.  We *must* open this. */
+	f = fopen(name = options->config_filename, "r");
+	if (!f) {
+	    fail(2, "unable to open file: \"%s\"", name);
+	}
+    }
+    else {
+	/* Try each of the standard config file locations in turn.  No
+	 * problem if we cannot open any of them. */
+		
+	if (!(f = fopen(name = "./" LOCAL_CONFIG_FILE, "r"))) {
+	    /* Nothing in the cur directory; try our home directory. */
+	    glob_t paths;
+	    int res;
+	    if ((res = glob("~/" LOCAL_CONFIG_FILE,
+			    GLOB_TILDE, NULL, &paths)) == 0)
+	    {
+		name = paths.gl_pathv[0];
+		STRCPY((*p_filename), name);
+	        f = fopen(name, "r");
+		globfree(&paths);
+		if (!f) {
+		    FREE(*p_filename);
+		    *p_filename = NULL;
+		}
+
+	    }
+	    else {
+		/* Nothing in our home directory; try the default config
+		 * file path. */ 
+		f = fopen(name = CONFIG_FILE, "r");
+	    }
+	}
+    }
+    if (f && !*p_filename) {
+	STRCPY((*p_filename), name);
+    }
+    return f;
+}
+
+
+/**
+ * @brief Read the next token and value pair from the config file.
+ * 
+ * Keep calling this until it returns NULL at which time \p f will have
+ * been closed.
+ *
+ * @param f (FILE *) The file pointer for the configuration file.
+ * @param p_token (char **) Pointer to a (char *) string variable
+ *        into which a dynamically allocated string will be placed for a
+ *        configuration token.  The caller must free this.
+ * @param p_value (char **) Pointer to a (char *) string variable
+ *        into which a dynamically allocated string will be placed for a
+ *        configuration value.  The caller must free this.
+ * 
+ * @return (FILE *) the file pointer for the config file until the last
+ *         config setting has been returned, and then NULL.
+ */
+extern FILE *
+next_config_setting(
+    FILE *f, char **p_token, char **p_value, int *p_line_no, char *filename)
+{
+    char c;
+    char *idx;
+    char *trailing_space = NULL;
+    bool in_comment = false;
+    bool before_text = true;
+    int len = 0;
+    int size = 100;
+    
+    *p_token = idx = (char *) MALLOC(size);
+    *p_value = NULL;
+
+    while ((c = fgetc(f)) != EOF) {
+	if (in_comment) {
+	    if (c != '\n') {
+		continue;
+	    }
+	}
+	switch (c) {
+	case '#':
+	    in_comment = true;
+	    continue;
+	case '=':
+	    *idx = '\0';
+	    *p_value = idx = (char *) malloc(size);
+	    before_text = true;
+	    if (trailing_space) {
+		*trailing_space = '\0';
+		trailing_space = NULL;
+	    }
+	    continue;
+	case '\n':
+	    in_comment = false;
+	    (*p_line_no)++;
+	    break;
+	default:
+	    if (before_text && isspace(c)) {
+		continue;
+	    }
+	    before_text = false;
+	    if (isspace(c)) {
+		if (!trailing_space) {
+		    trailing_space = idx;
+		}
+	    }
+	    else {
+		trailing_space = NULL;
+	    }
+	    *idx = c;
+	    idx++;
+	    len++;
+	    if (len >= size) {
+		size = size + 200;
+		if (*p_value) {
+		    *p_value = realloc((void *) *p_value, size);
+		    idx = *p_value + len;
+		}
+		else {
+		    *p_token = realloc((void *) *p_token, size);
+		    idx = *p_token + len;
+		}
+	    }
+	    continue;
+	}
+	/* Only get here at the end of a line. */
+	*idx = '\0';
+	if (trailing_space) {
+	    *trailing_space = '\0';
+	}
+	if (idx == *p_token) {
+	    /* We read nothing, so go on to the next line. */
+	    continue;
+	}
+	if (!*p_value) {
+	    /* We have no value.  That is an error. */
+	    fail(0, "Invalid configuration entry at %s:%d\n"
+		 "\"%s\"\n", *p_line_no, filename, *p_token);
+	    FREE(*p_token);
+	    closedown(2);
+	}
+	return f;
+    }
+    
+    /* We reach this point only when eof has been reached, and there is
+     * nothing to return to the caller. */
+    fclose(f);
+    return NULL;
+}
+
+static cfg_option_t cfg_options[] = {
+    {CFG_NAME_VOLCURVE,  BOOLEAN},
+    {CFG_NAME_MAX_PCT,  INTEGER},
+    {CFG_NAME_ALSA_MIXER_NAME,  STRING},
+    {CFG_NAME_MPD_MIXER,  STRING},
+    {CFG_NAME_ALSA_CARD,  STRING},
+    {NULL, NONE}
+};
+
+static int
+get_option(const char *name)
+{
+    cfg_option_t *opt = cfg_options;
+    int idx = 0;
+    while (opt->option_name) {
+	if (strcmp(name, opt->option_name) == 0) {
+	    return idx;
+	}
+	opt++;
+	idx++;
+    }
+    return -1;
+}
+
+/**
+ * @brief Convert a string to lower case
+ * 
+ * @param str (char *) The string to downcase.
+ */
+static void
+downcase(char *str)
+{
+    int i;
+    for (i = strlen(str); i--; i >= 0) {
+	str[i] = tolower(str[i]);
+    }
+}
+
+/**
+ * @brief Read our config file, updating the #options variable.
+ * 
+ * @param options (option_t *) Pointer to our options struct.
+ */
+extern void
+read_config_file(options_t *options)
+{
+    char *filename = NULL;
+    FILE *f = open_config_file(options, &filename);
+    char *token;
+    char *value;
+    char  c;
+    bool  bval;
+    int   ival;
+    int   line_no;
+    int   opt_id;
+    
+    while (f &&
+	   (f = next_config_setting(f, &token, &value, &line_no, filename)))
+    {
+	printf("Token: %s, Value: \"%s\"\n", token, value);
+	downcase(token);
+	opt_id = get_option(token);
+	if (opt_id >= 0) {
+	    switch (cfg_options[opt_id].option_type) {
+	    case BOOLEAN:
+		downcase(value);
+		bval = ((strcmp(value, "yes") == 0) ||
+			(strcmp(value, "true") == 0));
+		if (!bval) {
+		    if (!((strcmp(value, "no") == 0) ||
+			  (strcmp(value, "false") == 0)))
+		    {
+			fail(3,
+			     "Invalid value (%s) for boolean \"%s\" at %s:%d",
+			     value, token, filename, line_no);
+		    }
+		}
+		break;
+	    case INTEGER:
+		ival = 0;
+		while (c = *value) {
+		    if ((c >= '0') && (c <= '9')) {
+			ival *= 10;
+			ival += (c - '0');
+		    }
+		    else {
+			fail(3,
+			     "Invalid value (%s) for integer \"%s\" at %s:%d",
+			     value, token, filename, line_no);
+		    }
+		}
+	    }
+	    switch (opt_id) {
+	    case 0:
+		options->volcurve = bval;
+		FREE(value);
+		break;
+	    case 1:
+		options->max_pct = ival;
+		FREE(value);
+		break;
+	    case 2:
+		options->alsa_mixer_name = value;
+		break;
+	    case 3:
+		options->mpd_mixer = value;
+		break;
+	    case 4:
+		options->alsa_card = value;
+		break;
+	    }
+	}
+	else {
+	    fail(3, "Unrecognized token \"%s\" at %s:%d", token,
+		 filename, line_no);
+	}
+    }
+    FREE(filename);
+}
+
+
+
+#endif
--- src/volumed.c
+++ src/volumed.c
@@ -33,15 +33,20 @@ It does this by:
     of invoking amixer as a shell command.
 */
 
+#define DOCONLY
+#include "config.c"
+#undef DOCONLY
+
 /*
  * PLAN:
- *   - read config file
- *   - valgrind makefile target
- *   - lint?
+ *   - check docs
+ *   - change some errors to warnings
  *   - unit tests
+ *   - valgrind
+ *   - lint?
  *   - create basic websocket service
  *   - create volumec
- *    - create volume-config-moode
+ *   - create volume-config-moode
  *   - man page?
  *   - debianize
  */
@@ -50,6 +55,7 @@ It does this by:
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
+#include <stdarg.h>
 #include "volumed.h"
 
 
@@ -59,7 +65,17 @@ It does this by:
  */
 static char *progname = NULL;
 
-static options_t options = {8888, 0};
+static options_t options = {
+    8888, 			/* default websocket port */
+    0,    			/* default verbosity */
+    NULL,			/* config file name */
+    CONFIG_VOLCURVE,
+    CONFIG_MAX_PCT,
+    CONFIG_ALSA_MIXER_NAME,
+    CONFIG_MPD_MIXER,
+    CONFIG_ALSA_CARD
+};
+
 
 /**
  * @brief Safely close down \ref index, returning \p exitcode.
@@ -69,10 +85,11 @@ static options_t options = {8888, 0};
  * Close down and free all resources used by \ref index before exitting
  * with exitcode.
  */
-static void
+extern void
 closedown(int exitcode)
 {
     free(progname);
+    FREE(options.config_filename);
     exit(exitcode);
 }
 
@@ -86,9 +103,14 @@ static void
 usage(int exitcode)
 {
     fprintf(stderr,
-	    "usage: %s [--verbose] [--port port-number]\n"
-	    "       port-number: the port on which the websocket "
-	    "is to be created.\n\n" , progname);
+	    "usage: %s [-v | --verbose] [(-p | --port) port-number]\n"
+	    "        [(-c | --config) config-file] [-V | --version]\n"
+	    "    port-number: the port on which the websocket "
+	    "is to be created\n"
+	    "                 (default - %d);\n"
+	    "    config-file: the name of a configuration file to use\n"
+	    "                 (default - \"%s\").\n"
+	    "\n" , progname, DEFAULT_PORT, CONFIG_FILE);
     closedown(exitcode);
 }
 
@@ -104,23 +126,6 @@ record_progname(char **argv)
     char *pname = (char *) malloc(strlen(argv[0]) + 1);
     strcpy(pname, argv[0]);
     progname = pname;
-}
-
-/**
- * @brief Record the port number of the websocket to be opened.
- *
- * @param optarg (char *) The string representing the port number.
- */
-static void
-record_port(char *optarg)
-{
-    options.port = atoi(optarg);
-    if ((options.port <= 0) || (options.port > 65535)) {
-	fprintf(stderr,
-		"%s: port must be a number in the range 1 .. 65535\n",
-		progname);
-	usage(2);
-    }
 }
 
 /**
@@ -144,43 +149,84 @@ show_version_and_exit()
 static void
 process_args(int argc, char **argv)
 {
-    static struct option option_defs[] = {
+    struct option option_defs[] = {
 	{"port", required_argument, NULL, 0},
 	{"verbose", no_argument, NULL, 0},
 	{"version", no_argument, NULL, 0},
+	{"config", required_argument, NULL, 0},
 	{NULL, 0, NULL, 0}
     };
+    char option_map[] = {'p', 'v', 'V', 'c'};
     int c;
     int oidx = 0;
     
     record_progname(argv);
 
     while ((c = getopt_long(
-		argc, argv, "p:v", option_defs, &oidx)) != -1)
+		argc, argv, "c:p:vV", option_defs, &oidx)) != -1)
     {
+	if (c == 0) {
+	    /* Get the shortcode that matches the long option. */
+	    c = option_map[oidx];
+	}	    
 	switch (c) {
-	case 0:
-	    if (strcmp(option_defs[oidx].name, "port") == 0) {
-		record_port(optarg);
-	    }
-	    else if (strcmp(option_defs[oidx].name, "verbose") == 0) {
-		options.verbosity++;
-	    }
-	    else if (strcmp(option_defs[oidx].name, "version") == 0) {
-		show_version_and_exit();
-	    }
+	case 'c':
+	    FREE(options.config_filename);
+	    STRCPY(options.config_filename, optarg);
 	    break;
 	case 'p':
-	    record_port(optarg);
+	    options.port = atoi(optarg);
+	    if ((options.port <= 0) || (options.port > 65535)) {
+		fail(2, "port must be a number in the range 1 .. 65535");
+	    }
+	    break;
+	case 'V':
+	    show_version_and_exit();
 	    break;
 	case 'v':
 	    options.verbosity++;
 	    break;
 	case '?':
+	    usage(2);
 	    break;
 	}
     }
 }
+
+void *
+checked_malloc(size_t size, const char *file, int line)
+{
+    void *res = malloc(size);
+    if (!res) {
+	fail(2, "Unable to allocate memory of size %d at %s:%d",
+	     size, file, line);
+    }
+}
+
+/**
+ * @brief Print an error message and, possibly, exit with a failure code.
+ *
+ * If \p code is non-zero, we will exit with that as an error code.
+
+ * @param code (int) The errorcode with which to exit, if non-zero.
+ * @param fmt (char *)... Formatting parameters just as with printf and
+ *                        friends.
+ */
+void
+fail(int code, const char *fmt, ...)
+{
+    va_list argp;
+    fprintf(stderr, "%s: ", progname);
+    va_start(argp, fmt);
+    vfprintf(stderr, fmt, argp);
+    va_end(argp);
+    fprintf(stderr, "\n");
+
+    if (code) {
+	closedown(code);
+    }
+}
+
 
 /**
  * @brief Main entry point to \ref index.
@@ -192,6 +238,11 @@ int
 main(int argc, char **argv)
 {
     process_args(argc, argv);
+    read_config_file(&options);
     printf("Port: %d, Verbosity: %d\n", options.port, options.verbosity);
+    printf("volcurve: %d, max_pct: %d\n", options.volcurve, options.max_pct);
+    printf("alsa_mixer: %s, mpd_mixer: %s\n",
+	   options.alsa_mixer_name, options.mpd_mixer);
+    printf("alsa_card: %s\n", options.alsa_card);
     closedown(0);
 }
